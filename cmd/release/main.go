5be8ce1fb20dcb026b9272378688bbc91091f9d9
Alex Berryman <alexbman@gmail.com>
2022-01-20 00:16:28 -0600

    json logging in release command for migration management

--- cmd/release/main.go
+++ cmd/release/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"database/sql"
 	_ "github.com/lib/pq"
+	"github.com/rs/zerolog/log"
 	migrate "github.com/rubenv/sql-migrate"
-	"log"
 	"os"
 )
 
@@ -15,18 +15,18 @@ func main() {
 
 	DatabaseUrl := os.Getenv("DATABASE_URL")
 	if DatabaseUrl == "" {
-		log.Fatal("databaseUrl must be set")
+		log.Fatal().Msg("databaseUrl must be set")
 	}
 
 	db, err := sql.Open("postgres", DatabaseUrl)
 	if err != nil {
-		log.Fatal("cannot connect to database:", err)
+		log.Fatal().Err(err).Msg("cannot connect to database")
 	}
 
 	n, err := migrate.Exec(db, "postgres", migrations, migrate.Up)
 	if err != nil {
-		log.Fatal("migrations failed to up", err)
+		log.Fatal().Err(err).Msg("migrations failed to up")
 	}
 
-	log.Printf("Applied %d migrations!\n", n)
+	log.Info().Msgf("Applied %d migrations!", n)
 }
